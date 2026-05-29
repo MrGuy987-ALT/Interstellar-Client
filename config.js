fa8f7c962a7f271dc6af26dda5a28b984a7a47bb
MrGuy987 Alt <2030swang@gmail.com>
2026-05-29 06:36:15 +0000

    add more passwords

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     MrGuy987: "helloitisthySirGuytheGreat!!",
+    Lumember: "lumemberpassword",
   },
 };
 
